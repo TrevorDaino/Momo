dc0b866d92ab228324831eb8c4d1852ffca6e0cb
Trevor Daino <dainotrevor@gmail.com>
2015-01-06 21:02:02 -0500

    Final Momo code without limit switches

--- src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -4,9 +4,10 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-
 package edu.wpi.first.wpilibj.templates;
+
 import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -16,45 +17,81 @@ import edu.wpi.first.wpilibj.*;
  * directory.
  */
 public class RobotTemplate extends SimpleRobot {
+
     Joystick leftJoystick = new Joystick(1);
     Joystick rightJoystick = new Joystick(2);
-    RobotDrive robotDrive = new RobotDrive(4,3,2,1);
-    DoubleSolenoid climberPiston = new DoubleSolenoid(3,4);
+    RobotDrive robotDrive = new RobotDrive(4, 3, 2, 1);
+    DoubleSolenoid climberPiston = new DoubleSolenoid(3, 4);
+    DoubleSolenoid magazine = new DoubleSolenoid(1, 2);
     Relay compressorSpike = new Relay(1);
     DigitalInput digitalCompressor = new DigitalInput(1);
+    Jaguar backMotor = new Jaguar(6);
+    Jaguar frontMotor = new Jaguar(5);
+    Relay hammer = new Relay(6);
+
     /**
      * This function is called once each time the robot enters autonomous mode.
      */
     public void autonomous() {
-        
+
     }
 
     /**
      * This function is called once each time the robot enters operator control.
      */
     public void operatorControl() {
-        
-        
-        
-        
         robotDrive.setSafetyEnabled(true);
-        while(isOperatorControl() && isEnabled()){
+        while (isOperatorControl() && isEnabled()) {
             robotDrive.tankDrive(-leftJoystick.getY(), -rightJoystick.getY());
-            if(rightJoystick.getRawButton(6)){
+            if (rightJoystick.getRawButton(6)) {
                 climberPiston.set(DoubleSolenoid.Value.kForward);
             }
-            if(rightJoystick.getRawButton(7)){
+            if (rightJoystick.getRawButton(7)) {
                 climberPiston.set(DoubleSolenoid.Value.kReverse);
             }
-            if(rightJoystick.getRawButton(8)){
+            if (rightJoystick.getRawButton(8)) {
                 climberPiston.set(DoubleSolenoid.Value.kOff);
             }
-            if(!digitalCompressor.get()){
+            if (!digitalCompressor.get()) {
                 compressorSpike.set(Relay.Value.kForward);
-            } else{
+            } else {
                 compressorSpike.set(Relay.Value.kOff);
+            }
+            if (rightJoystick.getRawButton(11)) {
+                magazine.set(DoubleSolenoid.Value.kForward);
+            }
+            if (rightJoystick.getRawButton(10)) {
+                magazine.set(DoubleSolenoid.Value.kReverse);
+            }
+            double transformedThrottleRight = transformThrottle(rightJoystick.getZ());
+            double transformedThrottleLeft = transformThrottle(leftJoystick.getZ());
+            System.out.print(rightJoystick.getZ() + "--" + transformedThrottleRight);
+            System.out.println("        " + leftJoystick.getZ() + "--" + transformedThrottleLeft);
+            SmartDashboard.putNumber("Left throttle:", transformedThrottleLeft);
+            SmartDashboard.putNumber("Right throttle:", transformedThrottleRight);
+            if (transformedThrottleLeft > 0.1) {
+                backMotor.set(transformedThrottleLeft);
+            } else {
+                backMotor.set(0);
+            }
+            if (transformedThrottleRight > 0.1) {
+                frontMotor.set(transformedThrottleRight);
+            } else {
+                frontMotor.set(0);
+            }
+            if(rightJoystick.getRawButton(1)){
+                hammer.set(Relay.Value.kReverse);
+            } else if(rightJoystick.getRawButton(2)){
+                hammer.set(Relay.Value.kForward);
+            } else{
+                hammer.set(Relay.Value.kOff);
             }
             Timer.delay(0.01);
         }
+    }
+
+    public double transformThrottle(double throttle) {
+        double transformedThrottle = ((throttle * -1) + 1) / 2;
+        return transformedThrottle;
     }
 }
